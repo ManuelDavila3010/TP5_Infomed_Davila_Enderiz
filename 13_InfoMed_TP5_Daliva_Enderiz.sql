a84a7a8a8a28b0f78822ea1a1598fcc77e1aafd5
rocioenderiz <106761216+rocioenderiz@users.noreply.github.com>
2024-10-23 07:50:03 -0300

    Delete 13_InfoMed_TP5_Daliva_Enderiz.sql

--- 13_InfoMed_TP5_Daliva_Enderiz.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    P.nombre AS nombre_paciente,
-    COUNT(R.id_receta) AS total_recetas
-FROM 
-    Pacientes P
-LEFT JOIN 
-    Recetas R ON P.id_paciente = R.id_paciente
-GROUP BY 
-    P.id_paciente;
